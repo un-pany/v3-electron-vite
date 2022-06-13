6837ee172dd49249a488380d78827ce9eb5af416
pany <939630029@qq.com>
2022-06-13 18:25:21 +0800

    fix: 修改全局 key

--- src/renderer/constant/key.ts
+++ src/renderer/constant/key.ts
@@ -1,7 +1,7 @@
 class Keys {
-    static sidebarStatus = 'v3-admin-vite-sidebar-status-key'
-    static token = 'v3-admin-vite-token-key'
-    static activeThemeName = 'v3-admin-vite-active-theme-name-key'
+    static sidebarStatus = 'v3-electron-vite-sidebar-status-key'
+    static token = 'v3-electron-vite-token-key'
+    static activeThemeName = 'v3-electron-vite-active-theme-name-key'
 }
 
 export default Keys
