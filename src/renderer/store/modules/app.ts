971b96bd80242071a177be826b774b4ed432d98a
pany <939630029@qq.com>
2022-06-13 17:47:50 +0800

    feat: 适配 Element Plus v2.2.2

--- src/renderer/store/modules/app.ts
+++ src/renderer/store/modules/app.ts
@@ -54,13 +54,13 @@ export const useAppStore = defineStore({
             // 检查这个主题在主题列表里是否存在
             this.activeThemeName = this.themeList.find((theme) => theme.name === activeThemeName) ? activeThemeName : this.themeList[0].name
             // 应用到 dom
-            document.body.className = `theme-${this.activeThemeName}`
+            document.documentElement.className = this.activeThemeName
             // 持久化
             setActiveThemeName(this.activeThemeName)
         },
         initTheme() {
             // 初始化
-            document.body.className = `theme-${this.activeThemeName}`
+            document.documentElement.className = this.activeThemeName
         }
     }
 })
