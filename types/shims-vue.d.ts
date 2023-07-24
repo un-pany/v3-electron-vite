5eb30b8f7c3366521040d2a226bcae241a4932dd
pany <939630029@qq.com>
2023-07-24 13:42:41 +0800

    chore: 更新 vite/client 配置

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -1,10 +1,6 @@
-/// <reference types="vite/client" />
-
-declare module "*.vue" {
-  import { DefineComponent } from "vue"
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.scss" {
+  const scss: Record<string, string>
+  export default scss
 }
 
 declare module "virtual:*" {
